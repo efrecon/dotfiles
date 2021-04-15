73c19f1a890f74c6a52cbf7cf6caf35a5d7ec3fe
Emmanuel Frecon <efrecon@gmail.com>
2021-04-15 14:19:03 +0200

    Migrated improved settings for vs code

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -7,6 +7,26 @@ code --install-extension ms-vscode-remote.remote-containers
 code --install-extension ms-vscode-remote.remote-ssh
 code --install-extension ms-vscode-remote.remote-ssh-edit
 code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension mrmlnc.vscode-apache
+code --install-extension tamasfe.even-better-toml
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension zamerick.vscode-caddyfile-syntax
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension janisdd.vscode-edit-csv
+code --install-extension golang.go
+code --install-extension casualjim.gotemplate
+code --install-extension wholroyd.hcl
+code --install-extension sirtori.indenticator
+code --install-extension brunnerh.insert-unicode
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension darkriszty.markdown-table-prettify
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension timonwong.shellcheck
+code --install-extension bkromhout.vscode-tcl
+code --install-extension shardulm94.trailing-spaces
+code --install-extension redhat.vscode-yaml
 
 # Themes
 code --install-extension arcticicestudio.nord-visual-studio-code
+code --install-extension eamodio.amethyst-theme

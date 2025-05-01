377451eaed62095343a17148dface0b58e64c557
Emmanuel Frécon <efrecon@gmail.com>
2025-05-01 13:54:08 +0200

    Use gnome shell directory as flag

--- bash/.bashrc.d/gnome.sh
+++ bash/.bashrc.d/gnome.sh
@@ -1,34 +1,37 @@
 #!/usr/bin/bash
 
 XDG_DATA_HOME=${XDG_DATA_HOME:-${HOME}/.local/share}
-GNOME_EXTENSIONS_INDEX=${GNOME_EXTENSIONS_INDEX:-${XDG_DATA_HOME}/gnome-shell/extensions/.unforge}
+GNOME_SHELL_DIR=${GNOME_SHELL:-${XDG_DATA_HOME}/gnome-shell}
+GNOME_EXTENSIONS_INDEX=${GNOME_EXTENSIONS_INDEX:-${GNOME_SHELL_DIR}/extensions/.unforge}
 
-if ps -e | grep -Fq gnome-shell | grep -v grep; then
-    if [ -f "$GNOME_EXTENSIONS_INDEX" ]; then
-        while IFS= read -r line || [ -n "${line:-}" ]; do
-            # Skip leading comments and empty lines.
-            if [ "${line#\#}" != "$line" ]; then
-                continue
-            fi
-            if [ -n "$line" ]; then
-                extension=$(printf %s\\n "$line" | awk '{print $1}')
-                if [ -d "$(dirname "$GNOME_EXTENSIONS_INDEX")/$extension" ]; then
-                    # Recompile extensions schemas
-                    if [ -d "$(dirname "$GNOME_EXTENSIONS_INDEX")/${extension}/schemas" ]; then
-                        if gnome-extensions list --inactive --user | grep -Fq "$extension"; then
-                            glib-compile-schemas "$(dirname "$GNOME_EXTENSIONS_INDEX")/${extension}/schemas"
+if [ -d "$GNOME_SHELL_DIR" ]; then
+    if ps -e | grep -Fq gnome-shell | grep -v grep; then
+        if [ -f "$GNOME_EXTENSIONS_INDEX" ]; then
+            while IFS= read -r line || [ -n "${line:-}" ]; do
+                # Skip leading comments and empty lines.
+                if [ "${line#\#}" != "$line" ]; then
+                    continue
+                fi
+                if [ -n "$line" ]; then
+                    extension=$(printf %s\\n "$line" | awk '{print $1}')
+                    if [ -d "$(dirname "$GNOME_EXTENSIONS_INDEX")/$extension" ]; then
+                        # Recompile extensions schemas
+                        if [ -d "$(dirname "$GNOME_EXTENSIONS_INDEX")/${extension}/schemas" ]; then
+                            if gnome-extensions list --inactive --user | grep -Fq "$extension"; then
+                                glib-compile-schemas "$(dirname "$GNOME_EXTENSIONS_INDEX")/${extension}/schemas"
+                            fi
+                        fi
+                        # And enable the extension
+                        if gnome-extensions list --disabled --user | grep -Fq "$extension"; then
+                            gnome-extensions enable --quiet "$extension"
                         fi
                     fi
-                    # And enable the extension
-                    if gnome-extensions list --disabled --user | grep -Fq "$extension"; then
-                        gnome-extensions enable --quiet "$extension"
-                    fi
                 fi
-            fi
-        done < "$GNOME_EXTENSIONS_INDEX"
+            done < "$GNOME_EXTENSIONS_INDEX"
+        fi
+    else
+        (
+            sleep 5 && "$HOME/.bashrc.d/gnome.sh"
+        ) &
     fi
-else
-    (
-        sleep 5 && "$HOME/.bashrc.d/gnome.sh"
-    ) &
 fi

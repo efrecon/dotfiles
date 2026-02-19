0fabea448fd1c35ce8f022c7ca793dcfd25ba0dd
Emmanuel Frécon <efrecon@gmail.com>
2026-02-19 16:11:32 +0100

    🐛 Prevent using service, old WSL2 construct

--- bash/.bashrc.d/wsl2.sh
+++ bash/.bashrc.d/wsl2.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
 if uname -r | grep -q WSL2; then
-    # Start docker daemon if not already running.
-    service docker status > /dev/null || sudo service docker start
+    # Start docker daemon if not already running. This is needed on older
+    # versions of WSL2 that did not support systemd.
+    if command -v service > /dev/null; then
+        service docker status > /dev/null || sudo service docker start
+    fi
 
     # Copy Windows SSH keys into Linux VM
     if [ -f "${HOME}/bin/wsl2/ssh-copy.sh" ]; then
